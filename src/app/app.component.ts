d024b28da8644f5a328402879c48ff4d247989cb
khanhhuynguyenvu <khanhhuynguyenvu@gmail.com>
2020-04-06 00:14:47 +0700

    add http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly ROOT_URL = 'https://app-be-sk.herokuapp.com';
+  posts: any;
+  onPost: boolean;
+  obj: any;
+
+  constructor(private http: HttpClient) {
+    this.onPost = false;
+  }
+
   title = 'appFeSK';
+
+  getPost(postId: number) {
+    this.posts = null;
+    this.http.get(this.ROOT_URL + '/get/' + postId.toString(), {responseType: 'json'}).subscribe(data => {
+      this.posts = data;
+      console.log('Data: ', data);
+    });
+    this.onPost = true;
+    // console.log('log:', this.posts);
+  }
 }
